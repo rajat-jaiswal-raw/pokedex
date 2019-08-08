79fc2dc470d5fae3c07dcf51c1b8a6184fd31e1f
rajat-jaiswal-raw <rajat.jaiswal@raweng.com>
2019-08-08 13:09:21 +0530

    Updates

---
+++ precache-manifest.cf9187caac265f0cdaa889e83cb1bc89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "942dd10aceaaff4093a02d5e831e5104",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "cc759d1059b5f5bf7474",
+    "url": "/pokedex/static/css/main.fb1b9618.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/pokedex/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "cc759d1059b5f5bf7474",
+    "url": "/pokedex/static/js/main.fd9b2f39.chunk.js"
+  },
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  }
+]);
